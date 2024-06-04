0cc369877d23dd05daee13d3b7d9fdcbbd0a40f2
Adam Aho <56700218+rtkaaho@users.noreply.github.com>
2024-06-04 12:17:32 -0400

    feat: add harpoon, and some keymaps for moving code around

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Enter Normal Mode" })
+
+-- Move blocks of selected code up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move block down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move block up" })
