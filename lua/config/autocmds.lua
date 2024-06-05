637fa8a149891f5e62863b40a19565d0f1a11b79
adamaho <aho.adam01@gmail.com>
2024-06-04 22:41:29 -0400

    feat: disable autoformat for all javascript related buffers
    - Enable prettier
    - Enable astro

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+-- Turn off autoformat for any ts or js related files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "lua", "typescriptreact", "typescript", "astro", "svelte", "javascript", "javascriptreact" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
