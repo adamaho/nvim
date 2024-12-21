124a23da6571684b51a210ff244c1f612eb5b2b7
adamaho <aho.adam01@gmail.com>
2024-12-21 13:40:52 -0500

    lazy updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,8 @@ vim.keymap.set("i", "jj", "<Esc>", { desc = "Enter Normal Mode" })
 -- Move blocks of selected code up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move block down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move block up" })
+
+-- Swap the cwd and root commands for telescope 
+vim.keymap.set("n", "<leader><space>", LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)" })
+vim.keymap.set("n", "<leader>fF", LazyVim.pick("files", { root = true }), { desc = "Find Files (root)" })
+
